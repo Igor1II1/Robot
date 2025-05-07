dcf8b80744f3e44c09d55ed40427b7b21cc117b9
Igor Zelenskii <jmuxjmux5@gmail.com>
2025-05-07 14:17:37 +1000

    Project Robot

---
+++ src/home/HomePage.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="home">
+		<div>
+			<img class="robot" src="../assets/robot-home.png" />
+		</div>
+		<div><router-link to="/build">начни</router-link>собирать РОБОТА</div>
+	</div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.home {
+	text-align: center;
+}
+.robot {
+	height: 300px;
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,28 @@
+import RobotBuilder from '@/build/RobotBuilder.vue'
+import HomePage from '@/home/HomePage.vue'
+import PartInfo from '@/parts/partInfo.vue'
+import { createRouter, createWebHashHistory } from 'vue-router'
+
+//прописываем , если омпонент не подлючился автоматически
+// import HomePage from '@/home/HomePage.vue'
+
+export default createRouter({
+	history: createWebHashHistory(),
+	routes: [
+		{
+			path: '/',
+			name: 'главная',
+			component: HomePage,
+		},
+		{
+			path: '/build',
+			name: 'построй бота',
+			component: RobotBuilder,
+		},
+		{
+			path: '/parts/partType/:id',
+			name: 'ЗАПЧАСТИ',
+			component: PartInfo,
+		},
+	],
+})
